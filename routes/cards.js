e07e033ccd0df89547b44ed1ae19911457f1ac9d
Nikita Tarasenko <skaerlll@gmail.com>
2020-07-30 20:00:37 +0300

    Added server error hanling if json docs are not existed + fixed linter issues

--- routes/cards.js
+++ routes/cards.js
@@ -2,13 +2,18 @@ const cardsRouter = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 
-const reader = fs.createReadStream(path.join(__dirname, '../data/cards.json'), { encoding: 'utf8' });
-
 cardsRouter.get('/cards', (request, response) => {
-  reader.on('data', (data) => response.write(data));
-  reader.on('end', () => response.end());
-  reader.on('error', (error) => response.status(500).send({ message: 'Server error' }));
-  //reader.pipe(response);
+  try {
+    const reader = fs.createReadStream(path.join(__dirname, '../data/cards.json'), { encoding: 'utf8' });
+
+    reader.on('data', (data) => response.write(data));
+    reader.on('end', () => response.end());
+    reader.on('error', () => response.status(500).send({ message: 'Ошибка сервера' }));
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+    response.status(500).send({ message: 'Ошибка сервера' });
+  }
 });
 
 module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -3,25 +3,26 @@ const path = require('path');
 const fs = require('fs');
 
 let users = [];
+let serverError = false;
 
 fs.readFile(path.join(__dirname, '../data/users.json'), (error, data) => {
   if (error) {
-    // eslint-disable-next-line no-console
-    console.log(error);
+    serverError = true;
     return;
   }
   users = JSON.parse(data);
 });
 
 usersRouter.get('/users', (request, response) => {
-  response.send(users);
+  // eslint-disable-next-line no-unused-expressions
+  serverError ? response.status(500).send({ message: 'Ошибка сервера' }) : response.send(users);
 });
 
 usersRouter.get('/users/:id', (request, response) => {
   // eslint-disable-next-line no-underscore-dangle
   const currentUser = users.find((item) => item._id === request.params.id);
   if (!currentUser) {
-    response.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+    response.status(404).send({ message: 'Нет пользователя с таким id' });
     return;
   }
   response.send(currentUser);
